f1f971fea320621e7dfdb3ab6ee1d39f557765c9
Matt Kline <matt@bitbashing.io>
2022-12-09 23:29:59 -0800

    Day 10

---
+++ hs/day10.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import qualified Data.ByteString as BS(readFile)
+import Data.Foldable as F
+import Data.List.Split
+import Data.Text(Text)
+import qualified Data.Text as T
+import Data.Text.Encoding(decodeUtf8)
+import Data.Int
+import System.Environment
+
+data Instruction = Add Int32 | Noop deriving (Show)
+
+data State = State {
+    register :: Int32,
+    elapsed :: Int8
+} deriving (Show)
+
+initial :: State
+initial = State 1 0
+
+tick :: State -> [Instruction] -> (State, [Instruction])
+tick s [] = (s, [])
+tick State{..} (i:next) = case i of
+    Noop -> (State register 0, next)
+    Add x -> if elapsed > 0
+        then (State (register + x) 0, next)
+        else (State register (elapsed + 1), i:next)
+
+nthCycle :: [Instruction] -> [(State, [Instruction])]
+nthCycle instrs = takeWhile (\(_, i) -> not $ F.null i) cycles where
+    cycles = iterate (uncurry tick) (initial, instrs)
+
+parseInstructions :: Text -> [Instruction]
+parseInstructions input = parseInstruction <$> T.lines input
+
+parseInstruction :: Text -> Instruction
+parseInstruction line
+    | line == T.pack "noop" = Noop
+    | head toks == T.pack "addx" = Add . read . T.unpack . last $ toks
+    | otherwise = error $ "unknown instruction " <> T.unpack line
+        where toks = T.words line
+
+part1 :: [Instruction] -> Int32
+part1 input = sum $ sigStrength input <$> [20, 60, 100, 140, 180, 220]
+
+sigStrength :: [Instruction] -> Int -> Int32
+sigStrength i n = fromIntegral n * register (fst (nthCycle i !! (n - 1)))
+
+data Coord = Coord {
+    cx :: Int32,
+    cy :: Int32
+} deriving (Show, Eq, Ord)
+
+part2 :: [Instruction] -> IO ()
+part2 input = do
+    let
+        -- Pull the register out of each cycle
+        registers = register . fst <$> nthCycle input
+        -- And index it by cycle
+        indexedRegisters = zip registers [0..]
+        -- The beam moves along the scanline and resets every 40 ticks
+        pitch = 40
+        beam t = t `mod` pitch
+        -- The pixel is on if it's inside the sprite (+/- 1 register val)
+        pixelOn (reg, t) = abs (beam t - reg) <= 1
+        frame = pixelOn <$> indexedRegisters :: [Bool]
+        -- Draw our frame.
+        scanline s = do
+            traverse_ (\x -> putChar $ if x then '#' else '.') s
+            putChar '\n'
+    traverse_ scanline $ chunksOf (fromIntegral pitch) frame
+
+main :: IO ()
+main = do
+    args <- getArgs
+    input <- parseInstructions . decodeUtf8 <$> BS.readFile (head args)
+    print $ part1 input
+    part2 input
